5c4c29c4e2374c508c2ebcea1b8fc7d770eaac7e
DoQuocBinh <binhdq@fpt.edu.vn>
2020-11-24 10:30:55 +0700

    view engine

--- WebServer/app.js
+++ WebServer/app.js
@@ -2,6 +2,13 @@ var express = require('express')
 var app = express();
 var path = require('path')
 
+//npm i handlebars consolidate --save
+const engines = require('consolidate');
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
+
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname) +  '/index.html');
 })
@@ -16,6 +23,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var fs = require('fs')
 var fileName = 'data.txt';
+
+app.get('/viewAll',(req,res)=>{
+    // res.write('<html>');
+    // res.write('<body>')
+    // res.write('<ul>')
+    // data = fs.readFileSync(fileName,'utf8');
+    // var animals = data.split(';');
+    // animals.shift();//remove the first element
+    //     res.write('<li>'+ element + '</li>')
+    // });
+    // res.write('</ul>')
+    // res.write('</body>')
+    // res.write('</html>');
+    data = fs.readFileSync(fileName,'utf8');
+    var animals = data.split(';');
+    animals.shift();//remove the first element
+    res.render('animals',{model:animals})
+
+})
 
 app.post('/addAnimal',(req,res)=>{
     var name = req.body.animalName;
