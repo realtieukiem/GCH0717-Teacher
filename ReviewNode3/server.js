13ecdca151c4a085bc6251daf6f060345ac50b31
DoQuocBinh <binhdq@fpt.edu.vn>
2020-12-01 12:14:01 +0700

    edit and update

--- ReviewNode3/server.js
+++ ReviewNode3/server.js
@@ -15,36 +15,14 @@ app.use(express.static(__dirname + '/public'))
 var fs = require('fs')
 var fileName = 'data.txt'
 app.get('/',(req,res)=>{
-    let fileContent = fs.readFileSync(fileName,"utf8");
-    let userText = fileContent.split('/');
-    //remove the first because it's empty
-    userText.shift();
-    let userJson = [];
-    userText.forEach(user => {
-        let eachU = {
-            'name': user.split(':')[0],
-            'city': user.split(':')[1]
-        }
-        userJson.push(eachU)
-    });
+    let userJson = readDataFromFile();
     res.render('all',{data:userJson})
 })
 
 app.get('/delete',(req,res)=>{
     let nameToDelete = req.query.name;
     //1.Load data from file to array
-    let fileContent = fs.readFileSync(fileName,"utf8");
-    let userText = fileContent.split('/');
-    //remove the first because it's empty
-    userText.shift();
-    let userJson = [];
-    userText.forEach(user => {
-        let eachU = {
-            'name': user.split(':')[0],
-            'city': user.split(':')[1]
-        }
-        userJson.push(eachU)
-    });
+    let userJson = readDataFromFile();
     //2.find user in array and delete
     let indexToRemove = -1;
     for(i=0;i<userJson.length;i++){
@@ -62,7 +40,61 @@ app.get('/delete',(req,res)=>{
     fs.writeFileSync(fileName, contentToSave);
     res.redirect('/');
 })
+app.get('/edit',(req,res)=>{
+    let nameToEdit = req.query.name;
+    //1.Load data from file to array
+    let userJson = readDataFromFile();
+    //2.find user in array to edit
+    let indexToEdit = -1;
+    for(i=0;i<userJson.length;i++){
+        if(userJson[i].name==nameToEdit){
+            indexToEdit = i;
+            break;
+        }
+    }
+    res.render('edit',{data:userJson[indexToEdit]})
+})
+app.post('/update',(req,res)=>{
+    let nameToUpdate = req.body.txtName;
+    let cityToUpdate = req.body.txtCity;
+     //1.Load data from file to array
+     let userJson = readDataFromFile();
+     //2.find user in array to update
+    let indexToEdit = -1;
+    for(i=0;i<userJson.length;i++){
+        if(userJson[i].name==nameToUpdate){
+            indexToEdit = i;
+            break;
+        }
+    }
+    //update array
+    userJson[indexToEdit].city = cityToUpdate;
+    //3.Save array to file
+    let contentToSave ='';
+    for(i=0;i<userJson.length;i++){
+        contentToSave += '/' + userJson[i].name + ':' + userJson[i].city;
+    }
+    fs.writeFileSync(fileName, contentToSave);
+    res.redirect('/');
+     
+})
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT);
 console.log('Server is running....')
+
+function readDataFromFile() {
+    let fileContent = fs.readFileSync(fileName, "utf8");
+    let userText = fileContent.split('/');
+    //remove the first because it's empty
+    userText.shift();
+    let userJson = [];
+    userText.forEach(user => {
+        let eachU = {
+            'name': user.split(':')[0],
+            'city': user.split(':')[1]
+        };
+        userJson.push(eachU);
+    });
+    return userJson;
+}
