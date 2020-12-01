1cc86662f0cdf6b42a5715b7d1cd269e601e6f88
DoQuocBinh <binhdq@fpt.edu.vn>
2020-12-01 10:50:11 +0700

    delete user

--- ReviewNode3/server.js
+++ ReviewNode3/server.js
@@ -29,6 +29,40 @@ app.get('/',(req,res)=>{
     });
     res.render('all',{data:userJson})
 })
+
+app.get('/delete',(req,res)=>{
+    let nameToDelete = req.query.name;
+    //1.Load data from file to array
+    let fileContent = fs.readFileSync(fileName,"utf8");
+    let userText = fileContent.split('/');
+    //remove the first because it's empty
+    userText.shift();
+    let userJson = [];
+    userText.forEach(user => {
+        let eachU = {
+            'name': user.split(':')[0],
+            'city': user.split(':')[1]
+        }
+        userJson.push(eachU)
+    });
+    //2.find user in array and delete
+    let indexToRemove = -1;
+    for(i=0;i<userJson.length;i++){
+        if(userJson[i].name==nameToDelete){
+            indexToRemove = i;
+            break;
+        }
+    }
+    userJson.splice(indexToRemove,1);
+    //3.Save array to file
+    let contentToSave ='';
+    for(i=0;i<userJson.length;i++){
+        contentToSave += '/' + userJson[i].name + ':' + userJson[i].city;
+    }
+    fs.writeFileSync(fileName, contentToSave);
+    res.redirect('/');
+})
+
 var PORT = process.env.PORT || 3000;
 app.listen(PORT);
 console.log('Server is running....')
