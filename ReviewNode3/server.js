daf1241125446800c0268979a73f1607d3665ec4
DoQuocBinh <binhdq@fpt.edu.vn>
2020-12-01 09:04:26 +0700

    aa

--- ReviewNode3/server.js
+++ ReviewNode3/server.js
@@ -8,6 +8,10 @@ app.set('view engine','hbs');
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//register folder public to store: js,css,images
+app.use(express.static(__dirname + '/public'))
+
 var fs = require('fs')
 var fileName = 'data.txt'
 app.get('/',(req,res)=>{
@@ -23,7 +27,7 @@ app.get('/',(req,res)=>{
         }
         userJson.push(eachU)
     });
-    res.render('all',{data:userJson});
+    res.render('all',{data:userJson})
 })
 var PORT = process.env.PORT || 3000;
 app.listen(PORT);
